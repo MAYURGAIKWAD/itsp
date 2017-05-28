d23572189d191756ea62cd3b6d6d050c1b9dfd7a
MAYURGAIKWAD <mayurgaikwad121@gmail.com>
2017-05-28 19:03:28 +0530

    Update FingerPath.java

--- drawing app/FingerPath.java
+++ drawing app/FingerPath.java
@@ -1,48 +1,25 @@
 package com.example.mayurgaikwad.paint;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.graphics.Path;
 
-public class MainActivity extends AppCompatActivity {
-private PaintView paintView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        paintView=(PaintView) findViewById(R.id.paintView);
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        paintView.init(metrics);
-    }
+/**
+ * Created by Mayur on 22-05-2017.
+ */
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater= getMenuInflater();
-        menuInflater.inflate(R.menu.main,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+public class FingerPath {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.normal:
-                paintView.normal();
-                return true;
-            case R.id.emboss:
-                paintView.emboss();
-                return true;
-            case R.id.blur:
-                paintView.blur();
-                return true;
-            case R.id.clear:
-                paintView.clear();
-                return true;
 
-        }
-        return super.onOptionsItemSelected(item);
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public FingerPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
     }
 }
